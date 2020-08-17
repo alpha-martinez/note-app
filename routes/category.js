d0d87f15ba7c844ca439a56e44cd0ad6b97d449f
alpha-martinez <martinezalphaa@gmail.com>
2020-08-17 19:10:03 -0400

    attempting to add a POST route for notes...not fully working :(

--- routes/category.js
+++ routes/category.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const db = require('../models');
 const passport = require('../config/ppConfig');
 const { route } = require('./auth');
+const { render } = require('ejs');
 
 //POST /files - create a new file with content 
 
 router.post('/', (req, res) => {
-    db.category.findOrCreate({
-        name: req.body.categoryName,
-        userId: req.params.id
+    db.notes.create({
+        title: req.body.title,
+        content: req.body.content,
+        categoryId: req.params.id
     }).then((post) => {
-        res.redirect('/')
+        res.redirect(`/show/${req.params.id}`)
     }).catch((error) => {
         console.log(error, 'error')
     })
 })
+
 
 
 //GET /files/:id - display a specific file and its content 
@@ -28,3 +31,13 @@ router.get('/:id', (res, req) => {
 
 //EDIT 
 
+
+
+// render.post('/', (reqm res) => {
+//     db.notes.findOrCreate({
+
+//     })
+// })
+
+
+module.exports = router;
