200367bd804fb6d8ba5df59f8f4b2dfdb6057c39
alpha-martinez <martinezalphaa@gmail.com>
2020-08-24 15:56:04 -0400

    error

--- server.js
+++ server.js
@@ -62,4 +62,8 @@ const server = app.listen(port, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${port} 🎧`);
 });
 
+app.get('*', (req, res) => {
+  res.render('error')
+})
+
 module.exports = server;
